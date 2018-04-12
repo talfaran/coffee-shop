e389a1e912ad06135ad243cf93dd208eadd6b865
Talfaran <tal1faran@gmail.com>
2018-04-12 23:09:40 +0300

    coffeeShop extention 3

--- main.js
+++ main.js
@@ -1,28 +1,52 @@
-let amount = prompt("please enter the amount of beans you would like to purchase");
+//let amount = prompt("please enter the amount of beans you would like to purchase");
+let cofeeType = prompt("enter you drink here");
 var coffeeShop = {
   beans: 40,
   money: 50,
 
   drinkRequirements: {
-    latte: 10,
-    americano: 5,
-    doubleShot: 15,
-    frenchPress: 12,
+    latte: {
+      price: 4,
+      beanRequirment: 10,
+    },
+    americano: {
+      price: 5,
+      beanRequirment: 8,
+    },
+    doubleShot: {
+      price: 15,
+      beanRequirment: 15,
+    },
+    frenchPress: {
+      price: 11,
+      beanRequirment: 12,
+    }
   },
 
-buySupplies: function (amountToPurchase) {
-  this.beans = this.beans + amountToPurchase;
-  this.money -= amountToPurchase*2;
-  alert("the coffee shop now has: " + this.beans + " beans and " + this.money +" money");
-}
+  buyDrink: function (coffeeObj, adrink) {
+    
+      if (coffeeObj.drinkRequirements[adrink]) {
+        this.money += coffeeObj.drinkRequirements[adrink].price;
+        makeDrink(coffeeObj, adrink);
+        alert("the coffee shop now has: " + this.beans + " beans and " + this.money + " money");
+      } else {
+        alert("we do not have this drink");
+      } 
+  },
+
+  buySupplies: function (amountToPurchase) {
+    this.beans = this.beans + amountToPurchase;
+    this.money -= amountToPurchase * 2;
+    alert("the coffee shop now has: " + this.beans + " beans and " + this.money + " money");
+  }
 
 };
 
-makeDrink = function (AnyCoffeeShop ,drinkType) {
-  if (AnyCoffeeShop.drinkRequirements[drinkType]) {
-    if (AnyCoffeeShop.beans - AnyCoffeeShop.drinkRequirements[drinkType] >= 0) {
+makeDrink = function (AnyCoffeeShop, drinkType) {
+  if (AnyCoffeeShop.drinkRequirements[drinkType].beanRequirment) {
+    if (AnyCoffeeShop.beans - AnyCoffeeShop.drinkRequirements[drinkType].beanRequirment >= 0) {
       alert("enjoy your drink!");
-      return AnyCoffeeShop.beans -= AnyCoffeeShop.drinkRequirements[drinkType];
+      return AnyCoffeeShop.beans -= AnyCoffeeShop.drinkRequirements[drinkType].beanRequirment;
     } else {
       return alert("there are not enough beans to make your drink");
     }
@@ -32,10 +56,11 @@ makeDrink = function (AnyCoffeeShop ,drinkType) {
 
 }
 
-coffeeShop.buySupplies(10); // for some reason, if I use the 'amount' var as prompt, the beans property understands it as a string type, and the money property has integer type.
+coffeeShop.buyDrink(coffeeShop, cofeeType)
+//coffeeShop.buySupplies(10); // for some reason, if I use the 'amount' var as prompt, the beans property understands it as a string type, and the money property has integer type.
 
-makeDrink(coffeeShop, "latte");
-makeDrink(coffeeShop, "americano");
-makeDrink(coffeeShop, "filtered");
-makeDrink(coffeeShop, "doubleShot");
-makeDrink(coffeeShop, "frenchPress");
+//makeDrink(coffeeShop, "latte");
+//makeDrink(coffeeShop, "americano");
+//makeDrink(coffeeShop, "filtered");
+//makeDrink(coffeeShop, "doubleShot");
+//makeDrink(coffeeShop, "frenchPress");
